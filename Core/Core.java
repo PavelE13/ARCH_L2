4cac3717c2cbbf6bdb1d110bd41dbbcbeb9bdaa0
Benny Elashkin <pavel.elashkin@gmail.com>
2023-09-10 12:56:03 +0300

    Thirth Commit

--- Core/Core.java
+++ Core/Core.java
@@ -1,18 +1,32 @@
 package Core;
 
 import Abstractions.ItemGenerator;
+import Rewards.Cf.CfGenerator;
+import Rewards.Gas.GasGenerator;
 import Rewards.Gold.GoldGenerator;
+import Rewards.Oil.OilGenerator;
+import Rewards.Tritium.TritiumGenerator;
 
 import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Core {
     public void run() {
         List<ItemGenerator> generatorList = new ArrayList<>();
         generatorList.add(new GoldGenerator());
-        //generatorList.add(new SilverGenerator());
-        ItemGenerator myGenerator = generatorList.get(0);
-        System.out.println(myGenerator.openReward());
+        generatorList.add(new CfGenerator());
+        generatorList.add(new GasGenerator());
+        generatorList.add(new OilGenerator());
+        generatorList.add(new TritiumGenerator());
+
+        Random random = new Random();
+
+        for (int i = 1; i <= 6; i++) {
+            int index = random.nextInt(generatorList.size());
+            ItemGenerator myGenerator = generatorList.get(index);
+            System.out.println(myGenerator.openReward());
+        }
     }
 }
